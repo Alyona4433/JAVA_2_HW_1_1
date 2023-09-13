ed07d6e5765eb9ca833978d739c44653382186e0
Alyona4433 <alena4433@gmail.com>
2023-09-13 21:13:28 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //Створюю обєкт класу для збереження інформації
+        Person person = new Person ("Alyona", "Bielova");
+
+        //Створюю обєкт json
+        Gson gson = new Gson();
+
+        //Конвертуємо обєкт у JSON рядок
+        String json = gson.toJson(person);
+
+        //Виводжу JSON рядок у консоль
+        System.out.println(json);
+
+
+    }
+
+}
